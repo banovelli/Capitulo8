04d8abd9ca195d45a2a483c3f41b71d779785545
banovelli <bnovelli@gmail.com>
2016-07-07 10:06:47 -0300

    Pescar

---
+++ VaPescar/VaPescar/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace VaPescar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.listHand = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonAsk = new System.Windows.Forms.Button();
+            this.textProgress = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBooks = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(11, 24);
+            this.textName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(177, 20);
+            this.textName.TabIndex = 1;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(192, 24);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(99, 19);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "Start the game!";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // listHand
+            // 
+            this.listHand.FormattingEnabled = true;
+            this.listHand.Location = new System.Drawing.Point(310, 24);
+            this.listHand.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listHand.Name = "listHand";
+            this.listHand.Size = new System.Drawing.Size(126, 329);
+            this.listHand.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(308, 7);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sua mão";
+            // 
+            // buttonAsk
+            // 
+            this.buttonAsk.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAsk.Location = new System.Drawing.Point(310, 357);
+            this.buttonAsk.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonAsk.Name = "buttonAsk";
+            this.buttonAsk.Size = new System.Drawing.Size(126, 19);
+            this.buttonAsk.TabIndex = 5;
+            this.buttonAsk.Text = "Pergunte por uma carta";
+            this.buttonAsk.UseVisualStyleBackColor = true;
+            this.buttonAsk.Click += new System.EventHandler(this.buttonAsk_Click);
+            // 
+            // textProgress
+            // 
+            this.textProgress.Location = new System.Drawing.Point(9, 60);
+            this.textProgress.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textProgress.Multiline = true;
+            this.textProgress.Name = "textProgress";
+            this.textProgress.ReadOnly = true;
+            this.textProgress.Size = new System.Drawing.Size(283, 210);
+            this.textProgress.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 44);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Progresso do jogo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 272);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Livro";
+            // 
+            // textBooks
+            // 
+            this.textBooks.Location = new System.Drawing.Point(9, 288);
+            this.textBooks.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBooks.Multiline = true;
+            this.textBooks.Name = "textBooks";
+            this.textBooks.ReadOnly = true;
+            this.textBooks.Size = new System.Drawing.Size(283, 88);
+            this.textBooks.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 384);
+            this.Controls.Add(this.textBooks);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textProgress);
+            this.Controls.Add(this.buttonAsk);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listHand);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Vá Pescar!!!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ListBox listHand;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonAsk;
+        private System.Windows.Forms.TextBox textProgress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBooks;
+    }
+}
+
---
+++ VaPescar/VaPescar/Game.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VaPescar
+{
+    public class Game
+    {
+        private List<Player> players;
+        private Dictionary<Card.Values, Player> books;
+        private Deck stock;
+        private TextBox textBoxOnForm;
+
+        public Game(string playerName, string[] opponentNames, TextBox textBoxOnForm)
+        {
+            Random random = new Random();
+            this.textBoxOnForm = textBoxOnForm;
+            players = new List<Player>();
+            players.Add(new Player(playerName, random, textBoxOnForm));
+            foreach (string player in opponentNames)
+                players.Add(new Player(player, random, textBoxOnForm));
+            books = new Dictionary<Card.Values, Player>();
+            stock = new Deck();
+            Deal();
+            players[0].SortHand();
+        }
+
+
+        public string[] GetPlayerCardNames()
+        {
+            return players[0].GetCardNames();
+        }
+
+        public string DescribePlayerHands()
+        {
+            string description = "";
+            for (int i = 0; i < players.Count; i++)
+            {
+                description += players[i].Name + " has " + players[i].CardCount;
+                if (players[i].CardCount == 1)
+                    description += " carta.\r\n";
+                else
+                    description += " cartas.\r\n";
+            }
+            description += "A pilha têm " + stock.Count + " cards left.";
+            return description;
+        }
+
+
+
+        private void Deal()
+        {
+            stock.Shuffle();
+            for (int i = 0; i < 5; i++)
+                foreach (Player player in players)
+                    player.TakeCard(stock.Deal());
+            foreach (Player player in players)
+                PullOutBooks(player);
+        }
+
+
+        public bool PlayOneRound(int selectedPlayerCard)
+        {
+            Card.Values cardToAskFor = players[0].Peek(selectedPlayerCard).Value;
+            for (int i = 0; i < players.Count; i++)
+            {
+                if (i == 0)
+                    players[0].AskForACard(players, 0, stock, cardToAskFor);
+                else
+                    players[i].AskForACard(players, i, stock);
+                if (PullOutBooks(players[i]))
+                {
+                    textBoxOnForm.Text += players[i].Name + " peça nova mão\r\n";
+                    int card = 1;
+                    while (card <= 5 && stock.Count > 0)
+                    {
+                        players[i].TakeCard(stock.Deal());
+                        card++;
+                    }
+                }
+                players[0].SortHand();
+                if (stock.Count == 0)
+                {
+                    textBoxOnForm.Text = "A pilha está sem cartas. Game over!\r\n";
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool PullOutBooks(Player player)
+        {
+            List<Card.Values> BooksPulled = player.PullOutBooks();
+            foreach (Card.Values value in BooksPulled)
+                books.Add(value, player);
+            if (player.CardCount == 0)
+                return true;
+            return false;
+        }
+
+
+        public string DescribeBooks()
+        {
+            string whoHasWhichBooks = "";
+            foreach (Card.Values value in books.Keys)
+                whoHasWhichBooks += books[value].Name + " tem o livro de "
+                    + Card.Plural(value) + "\r\n";
+            return whoHasWhichBooks;
+        }
+
+        public string GetWinnerName()
+        {
+            Dictionary<string, int> winners = new Dictionary<string, int>();
+            foreach (Card.Values value in books.Keys)
+            {
+                string name = books[value].Name;
+                if (winners.ContainsKey(name))
+                    winners[name]++;
+                else
+                    winners.Add(name, 1);
+            }
+            int mostBooks = 0;
+            foreach (string name in winners.Keys)
+                if (winners[name] > mostBooks)
+                    mostBooks = winners[name];
+            bool tie = false;
+            string winnerList = "";
+            foreach (string name in winners.Keys)
+                if (winners[name] == mostBooks)
+                {
+                    if (!String.IsNullOrEmpty(winnerList))
+                    {
+                        winnerList += " e ";
+                        tie = true;
+                    }
+                    winnerList += name;
+                }
+            winnerList += " com " + mostBooks + " livros";
+            if (tie)
+                return "Um empate entre " + winnerList;
+            else
+                return winnerList;
+        }
+    }
+}
